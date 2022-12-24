13a8c1100f54d9475046342b04026660f7dfe936
waiyanmt <waiymt001@mymail.sim.edu.sg>
2022-12-24 12:45:40 +0800

    cleaned up layout of welcome page

--- script/script.js
+++ script/script.js
@@ -1,11 +1,11 @@
 park = document.querySelector('#parkStatus')
 
 const time = new Date()
-const hour = time.getHours() 
+// const hour = time.getHours() 
 
-// const hour = 11
+const hour = 24
 if (hour >= 9 && hour <= 21) {
-  park.innerHTML = '<div style="background-color:#caffbf; padding: 10px 20px;border-radius:30px; color:green;border: 1px solid green;">Park is currently OPEN!</div>'
+  park.innerHTML = '<div style="background-color:#caffbf; padding: 10px 20px;border-radius:5px; color:green;border: 1px solid green;width:fit-content;margin-right:15px;">Park is currently OPEN!</div>'
 } else {
-  park.innerHTML = '<div style="background-color:#e85d04;padding:10px 30px; border-radius:50px;color:white;">Park is currently CLOSED! <br /> Opens from 9am to 9pm</div>'
+  park.innerHTML = '<div style="background-color:#e85d04;padding:10px 30px; border-radius:5px;color:white;width:fit-content;margin-right:15px;">Park is currently CLOSED! <br /> Opens from 9am to 9pm</div>'
 }
