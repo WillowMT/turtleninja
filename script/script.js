04a90b88d7c1eda06f5d0fd95091480734b9aeb9
waiyanmt <waiymt001@mymail.sim.edu.sg>
2022-12-24 14:28:58 +0800

    fixed park time

--- script/script.js
+++ script/script.js
@@ -1,9 +1,9 @@
 park = document.querySelector('#parkStatus')
 
 const time = new Date()
-// const hour = time.getHours() 
+const hour = time.getHours() 
 
-const hour = 24
+// const hour = 24
 if (hour >= 9 && hour <= 21) {
   park.innerHTML = '<div style="background-color:#caffbf; padding: 10px 20px;border-radius:5px; color:green;border: 1px solid green;width:fit-content;margin-right:15px;">Park is currently OPEN!</div>'
 } else {
