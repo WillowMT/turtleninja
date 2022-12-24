4da515af0a0642a3fcfefd87f60419d594d23d70
waiyanmt <waiymt001@mymail.sim.edu.sg>
2022-12-24 21:42:07 +0800

    abt page responsive, bg white in map, bg responsive in ticket carousel

--- script/script.js
+++ script/script.js
@@ -5,7 +5,7 @@ const hour = time.getHours()
 
 // const hour = 24
 if (hour >= 9 && hour <= 21) {
-  park.innerHTML = '<div style="background-color:#caffbf; padding: 10px 20px;border-radius:5px; color:green;border: 1px solid green;width:fit-content;margin-right:15px;">Park is currently OPEN!</div>'
+  park.innerHTML = '<div style="background-color:#caffbf; padding: 10px 20px;border-radius:5px; color:green;border: 1px solid green;width:fit-content;margin-right:15px;">Park is currently Open!</div>'
 } else {
-  park.innerHTML = '<div style="background-color:#e85d04;padding:10px 30px; border-radius:5px;color:white;width:fit-content;margin-right:15px;">Park is currently CLOSED! <br /> Opens from 9am to 9pm</div>'
+  park.innerHTML = '<div style="background-color:#e85d04;padding:10px 30px; border-radius:5px;color:white;width:fit-content;margin-right:15px;">Park is currently Closed! <br /> Opens from 9am to 9pm</div>'
 }
